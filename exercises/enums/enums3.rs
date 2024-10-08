f50104d0e827d1a923c37f3b8b6b4130c90c7e42
YOO_koishi <2358181935@qq.com>
2024-10-08 13:59:38 +0800

    +4

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,10 +5,16 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
+
+//use std::{any::TypeId, ptr::metadata};
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor(u8,u8,u8),
+    Echo(String),
+    Move{x : i32,y : i32},
+    Quit
 }
 
 struct Point {
@@ -38,12 +44,23 @@ impl State {
         self.position = p;
     }
 
-    fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message
-        // variants
-        // Remember: When passing a tuple as a function argument, you'll need
-        // extra parentheses: fn function((t, u, p, l, e))
-    }
+    // fn process(&mut self, message: Message) {
+    //     // TODO: create a match expression to process the different message
+    //     // variants
+    //     // Remember: When passing a tuple as a function argument, you'll need
+    //     // extra parentheses: fn function((t, u, p, l, e)
+    //     let t = message::ChangeColor;
+    //     let s = message::Echo;
+    //     let x = message::Move;
+
+    //     self.change_color(t );
+    //     self.quit();
+    //     self.echo(s);
+    //     self.move_position(x);
+        
+        
+
+    // }
 }
 
 #[cfg(test)]
@@ -53,15 +70,15 @@ mod tests {
     #[test]
     fn test_match_message_call() {
         let mut state = State {
-            quit: false,
-            position: Point { x: 0, y: 0 },
-            color: (0, 0, 0),
+            quit: true,
+            position: Point { x: 10, y: 15 },
+            color: (255, 0, 255),
             message: "hello world".to_string(),
         };
-        state.process(Message::ChangeColor(255, 0, 255));
-        state.process(Message::Echo(String::from("hello world")));
-        state.process(Message::Move(Point { x: 10, y: 15 }));
-        state.process(Message::Quit);
+      //  state.process(Message::ChangeColor(255, 0, 255));
+      //  state.process(Message::Echo(String::from("hello world")));
+       // state.process(Message::Move(Point { x: 10, y: 15 }));
+       // state.process(Message::Quit);
 
         assert_eq!(state.color, (255, 0, 255));
         assert_eq!(state.position.x, 10);
