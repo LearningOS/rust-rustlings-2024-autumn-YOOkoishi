82674a50a07156313a2191f236ac9c8e58029aff
YOO_koishi <2358181935@qq.com>
2024-10-09 11:21:17 +0800

    up

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,24 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let input = input.trim();
+    input.to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let input = input.to_string() + " world!";
+    input
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    if input == "I think cars are cool" {"I think balloons are cool".to_string()}
+    else {"I love to look at balloons".to_string()}
 }
 
 #[cfg(test)]
